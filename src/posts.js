cf927d8e6fdb48ed81178e0bdff0b13796a1906b
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-07-03 09:21:53 +0200

    initial commit

---
+++ src/posts.js
@@ -0,0 +1,78 @@
+import React from "react";
+import axios from "axios";
+
+export default class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      comments: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then(response => {
+      this.setState({ posts: response.data.slice(0, 20) });
+      this.loadComments(this.state.posts[this.state.posts.length - 1].id);
+    });
+  }
+
+  comment(postId, comment) {
+    axios
+      .post(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`, {
+        body: comment
+      })
+      .then(response => {
+        this.setState({ comments: this.state.comments.concat(response.data) });
+      });
+  }
+
+  loadComments(postId) {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+      .then(response => {
+        this.setState({ comments: response.data.slice(0, 10) });
+      });
+  }
+
+  render() {
+    const { posts, comments } = this.state;
+    return (
+      <React.Fragment>
+        <div>Posts</div>
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post" key={post.id}>
+              <div className="post-title">Title: {post.title}</div>
+              <div className="post-text">Text: {post.body}</div>
+            </div>
+          ))}
+        </div>
+        <div className="comment">
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              this.comment(posts[posts.length - 1].id, e.target.comment.value);
+            }}
+          >
+            <textarea
+              name="comment"
+              placeholder="write a comment"
+              cols="50"
+              rows="2"
+            />
+            <button className="btn">submit</button>
+          </form>
+          <div>Comments</div>
+          <div className="showComments">
+            {comments.map(comment => (
+              <div className="showComment" key={comment.id}>
+                <div>{comment.body}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
